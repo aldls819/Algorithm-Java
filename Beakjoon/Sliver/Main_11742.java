13879c487f86a3824c2d2a47ae2eec470a65c33a
JiEunJeung <110436005+aldls819@users.noreply.github.com>
2023-03-07 22:37:21 +0900

    Create Main_11742.java

---
+++ Beakjoon/Sliver/Main_11742.java
@@ -0,0 +1,24 @@
+package BeakJoon.src.sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_11724 {
+
+    static int n, m; //정점개수, 간선개수
+    static List<Integer>[] list; //그래프
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+    }
+
+}
